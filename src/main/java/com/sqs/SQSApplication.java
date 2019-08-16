079baa00328eb6baf5da415689fe2295d8815908
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2019-08-16 13:18:00 +0200

    sqs tests

---
+++ src/main/java/com/sqs/SQSApplication.java
@@ -0,0 +1,12 @@
+package com.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SQSApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SQSApplication.class, args);
+	}
+}
