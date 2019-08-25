54bb1651baa18634db3d31a38132b2f6b751dab8
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2019-08-25 11:33:58 +0200

    sns connnect maven and springboot

--- src/main/java/com/sqs/controller/SQSController.java
+++ src/main/java/com/sqs/controller/SQSController.java
@@ -1,7 +1,13 @@
 package com.sqs.controller;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,48 +15,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
-
 @Controller
 public class SQSController {
 	
 	private static final Logger log = LoggerFactory.getLogger(SQSController.class);
-	
-	@Value("${sqs.url}")
-	private String sqsURL;
+
+	@Value("${aws.sns.topic.demo.ARN}")
+	String snsTopicDemoARN;
+
+	private AmazonSNS amazonSNS;
+
+	@Autowired
+	public SQSController() {
+		this.amazonSNS = AmazonSNSClientBuilder.standard().
+				withRegion(Regions.EU_CENTRAL_1)
+				.build();
+	}
 	
 	@RequestMapping(value = "/sendMessageQueue", method = RequestMethod.POST)
     public @ResponseBody void write(@RequestBody String notificationData){
 		try {
-		
-		final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
 		log.info("===============================================");
 		log.info("Getting Started with Amazon SQS Standard Queues");
 		log.info("===============================================\n");
         
 		log.info("Sending a message to MyQueue.\n");
-        sqs.sendMessage(new SendMessageRequest(sqsURL, notificationData));
+			PublishRequest publishRequest = new PublishRequest(snsTopicDemoARN, notificationData, "test");
+			PublishResult publishResult = this.amazonSNS.publish(publishRequest); ;
         log.info("Message Sent.\n");
-        
-		}catch (final AmazonServiceException ase) {
+
+		} catch (final Exception ase) {
 			log.error("Caught an AmazonServiceException, which means " +
                     "your request made it to Amazon SQS, but was " +
                     "rejected with an error response for some reason.");
 			log.error("Error Message:    " + ase.getMessage());
-			log.error("HTTP Status Code: " + ase.getStatusCode());
-			log.error("AWS Error Code:   " + ase.getErrorCode());
-			log.error("Error Type:       " + ase.getErrorType());
-			log.error("Request ID:       " + ase.getRequestId());
-        } catch (final AmazonClientException ace) {
-        	log.error("Caught an AmazonClientException, which means " +
-                    "the client encountered a serious internal problem while " +
-                    "trying to communicate with Amazon SQS, such as not " +
-                    "being able to access the network.");
-        	log.error("Error Message: " + ace.getMessage());
         }
     }
 }
